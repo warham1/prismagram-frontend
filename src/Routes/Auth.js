ddbc302cecfe596218bc85b25bd99cc9151f4e23
warham1 <yellow9697@hanyang.ac.kr>
2019-09-06 23:40:30 +0900

    signup and login

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,1 +1,85 @@
-export default () => "Auth";
+import React, { useState } from "react";
+import styled from "styled-components";
+import Input from "./../Components/Input";
+import Button from "../Components/Button";
+
+const Wrapper = styled.div`
+  min-height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Box = styled.div`
+  ${props => props.theme.whiteBox}
+  border-radius: 0px;
+  width: 100%;
+  max-width: 350px;
+`;
+
+const StateChanger = styled(Box)`
+  text-align: center;
+  padding: 20px 0px;
+`;
+
+const Link = styled.span`
+  color: ${props => props.theme.blueColor};
+  cursor: pointer;
+`;
+
+const Form = styled(Box)`
+  padding: 40px;
+  padding-bottom: 30px;
+  margin-bottom: 15px;
+  form {
+    width: 100%;
+    input {
+      width: 100%;
+      margin-bottom: 7px;
+    }
+    button {
+      margin-top: 10px;
+    }
+  }
+`;
+
+export default () => {
+  const [action, setAction] = useState("logIn");
+
+  return (
+    <Wrapper>
+      <Form>
+        {action === "logIn" ? (
+          <form>
+            <Input placeholder={"전화번호, 사용자 이름 또는 이메일"} />
+            <Input placeholder={"비밀번호"} />
+            <Button text={"로그인"} />
+          </form>
+        ) : (
+          <form>
+            <Input placeholder={"성"} />
+            <Input placeholder={"이름"} />
+            <Input placeholder={"휴대폰 번호 또는 이메일 주소"} />
+            <Input placeholder={"사용자 이름"} />
+            <Input placeholder={"비밀번호"} />
+            <Button text={"가입"} />
+          </form>
+        )}
+      </Form>
+      <StateChanger>
+        {action === "logIn" ? (
+          <>
+            계정이 없으신가요?{" "}
+            <Link onClick={() => setAction("signUp")}>가입하기</Link>
+          </>
+        ) : (
+          <>
+            계정이 있으신가요?{" "}
+            <Link onClick={() => setAction("logIn")}>로그인</Link>
+          </>
+        )}
+      </StateChanger>
+    </Wrapper>
+  );
+};
--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -1,16 +1,24 @@
-import { createGlobalStyle } from "styled-components"
-import reset from "styled-reset"
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 export default createGlobalStyle`
     ${reset};
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap');
+
     * {
         box-sizing:border-box;
     }
     body {
-        background-color: ${props=>props.theme.bgColor};
+        background-color: ${props => props.theme.bgColor};
         color: ${props => props.theme.blackColor};
+        font-size: 14px;
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
     a {
         color: ${props => props.theme.blueColor};
+        text-decoration: none;
     }
-`
+    input:focus {
+        outline:none
+    }
+`;
